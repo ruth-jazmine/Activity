37da2307f188b8a7a897ebd6015cd458d470ba57
ruth-jazmine-espiso <ruthjazmineespiso@gmail.com>
2025-05-02 07:20:13 +0000

    Task2

---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char h = 'H';
+        byte e = 3;          
+        short l = 1;         
+        int o = 0;  
+        char space = ' ';    
+        char w = 'w';        
+        char r = 'r';        
+        char d = 'd';        
+        float f = 2.0f;     
+        boolean bool = true;
+
+        String output = "" + h + e + l + l + o + space + w + o + r + l + d + space + f + space + bool;
+        System.out.println(output);
+    }
+}
