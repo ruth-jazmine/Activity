2ea952be41080390da993c6862acec5b13c80f4e
ruth-jazmine-espiso <ruthjazmineespiso@gmail.com>
2025-06-03 14:16:28 +0000

    Task16

---
+++ Task16.java
@@ -0,0 +1,42 @@
+interface Animal {
+    boolean feed(boolean feedGorilla);
+    void groom();
+    void pet();
+}
+
+class Gorilla implements Animal {
+
+    @Override
+    public boolean feed(boolean feedGorilla) {
+        if (feedGorilla) {
+            // put gorilla food into cage
+            System.out.println("Gorilla Feeding Time.");
+            return true;
+        } else {
+            System.out.println("Not Gorilla Feeding Time.");
+            return false;
+        }
+    }
+
+    @Override
+    public void groom() {
+        // lather, rinse, repeat
+        System.out.println("Gorilla Grooming Time.");
+    }
+
+    @Override
+    public void pet() {
+        // pet at your own risk
+        System.out.println("Pet Gorilla at your own risk.");
+    }
+}
+
+public class Task16 {
+    public static void main(String[] args) {
+        Gorilla g = new Gorilla();
+
+        g.feed(true);   // Gorilla Feeding Time.
+        g.groom();      // Gorilla Grooming Time.
+        g.pet();        // Pet Gorilla at your own risk.
+    }
+}
