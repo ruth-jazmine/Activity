e9ac4f48b15f7cefcd3ed2e055ca50fa2ae61e51
ruth-jazmine-espiso <ruthjazmineespiso@gmail.com>
2025-06-03 08:15:23 +0000

    Task10

--- Task10.java
+++ Task10.java
@@ -1,26 +1,24 @@
 public class Task10 {
-    class Student {
-    private String firstName;
-    private String lastName;
+    static class Student {
+        private String firstName;
+        private String lastName;
 
-    public Student(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        public Student(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public void printFullName() {
+            System.out.println(firstName + " " + lastName);
+        }
     }
 
-    // Method to print full name
-    public void printFullName() {
-        System.out.println(firstName + " " + lastName);
-    }
-}
-
-public class Task10 {
     public static void main(String[] args) {
         Student[] students = new Student[] {
             new Student("Morgan", "Freeman"),
             new Student("Brad", "Pitt"),
             new Student("Kevin", "Spacey"),
         };
+
         for (Student s : students) {
             s.printFullName();
         }
